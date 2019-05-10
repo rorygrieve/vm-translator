f8e28f1e686f37cabdcb95002bc1a4754e5c4ff5
Rory Grieve <rhrg500@gmail.com>
2019-05-10 17:34:37 +0100

    Use asm fixture for SimpleAdd
    This was only possible now because to test if the asm code worked
    required using the CPUEmulator that comes with the Nand2Tetris course.
    
    Therefore no fixtures were available to test drive the translation. The
    fixture is still useful however as it can be used to guard against
    regressions.

--- spec/features/translate_files_spec.rb
+++ spec/features/translate_files_spec.rb
@@ -5,23 +5,18 @@ require 'code_writer'
 RSpec.describe 'Translate files to assembly' do
   subject(:main) {
     Main.new(
-      parser: parser,
-      code_writer: code_writer,
+      parser: Parser,
+      code_writer: CodeWriter,
     )
   }
-  let(:parser) { class_double(Parser) }
-  let(:code_writer) { class_double(CodeWriter) }
 
-  before do
-    allow(parser).to receive(:call)
-    allow(code_writer).to receive(:call)
-  end
 
   context 'a file containing simple vm commands is added' do
     it 'is translated into assembly' do
       main.call('spec/fixtures/SimpleAdd.vm')
 
       expect(File).to exist('lib/output/SimpleAdd.asm')
+      expect(IO.read('lib/output/SimpleAdd.asm')).to eq(IO.read('spec/fixtures/SimpleAdd.asm'))
     end
   end
 
