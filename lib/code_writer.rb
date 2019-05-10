5501d6173d32c6b5ea3bb8e1cfe0aa605a437fa1
Rory Grieve <rhrg500@gmail.com>
2019-05-10 15:52:05 +0100

    Add support for add arithmetic commands

--- lib/code_writer.rb
+++ lib/code_writer.rb
@@ -10,15 +10,29 @@ class CodeWriter
 
   def self.convert(line)
     split_line = line.split
-    [
-      "// push constant #{split_line[2]}",
-      "@#{split_line[2]}",
-      'D=A',
-      '@SP',
-      'A=M',
-      'M=D',
-      '@SP',
-      'M=M+1',
-    ]
+
+    case split_line.length
+    when 3
+      [
+        "// push constant #{split_line[2]}",
+        "@#{split_line[2]}",
+        'D=A',
+        '@SP',
+        'A=M',
+        'M=D',
+        '@SP',
+        'M=M+1',
+      ]
+    when 1
+      [
+        '// add',
+        '@SP',
+        'M=M-1',
+        'A=M',
+        'D=M',
+        'A=A-1',
+        'M=D+M',
+      ]
+    end
   end
 end
--- spec/code_writer_spec.rb
+++ spec/code_writer_spec.rb
@@ -3,51 +3,74 @@ require 'code_writer'
 RSpec.describe CodeWriter do
   subject(:code_writer) { CodeWriter }
   describe '#call' do
-    context 'single push constant' do
-      let(:parsed_code) { ['push constant 8'] }
+    context 'memory access commands' do
+      context 'single push constant' do
+        let(:parsed_code) { ['push constant 8'] }
 
-      it 'converts it to assembly' do
-        expect(code_writer.call(parsed_code))
-          .to eq(
-            [
-              '// push constant 8',
-              '@8',
-              'D=A',
-              '@SP',
-              'A=M',
-              'M=D',
-              '@SP',
-              'M=M+1',
-            ]
-          )
+        it 'converts it to assembly' do
+          expect(code_writer.call(parsed_code))
+            .to eq(
+              [
+                '// push constant 8',
+                '@8',
+                'D=A',
+                '@SP',
+                'A=M',
+                'M=D',
+                '@SP',
+                'M=M+1',
+              ]
+            )
+        end
+      end
+
+      context 'mulitple push constants' do
+        let(:parsed_code) { ['push constant 8', 'push constant 9'] }
+
+        it 'converts it to assembly' do
+          expect(code_writer.call(parsed_code))
+            .to eq(
+              [
+                '// push constant 8',
+                '@8',
+                'D=A',
+                '@SP',
+                'A=M',
+                'M=D',
+                '@SP',
+                'M=M+1',
+                '// push constant 9',
+                '@9',
+                'D=A',
+                '@SP',
+                'A=M',
+                'M=D',
+                '@SP',
+                'M=M+1',
+              ]
+            )
+        end
       end
     end
 
-    context 'mulitple push constants' do
-      let(:parsed_code) { ['push constant 8', 'push constant 9'] }
+    context 'arithmetic commands' do
+      context 'add' do
+      let(:parsed_code) { ['add'] }
 
-      it 'converts it to assembly' do
-        expect(code_writer.call(parsed_code))
-          .to eq(
-            [
-              '// push constant 8',
-              '@8',
-              'D=A',
-              '@SP',
-              'A=M',
-              'M=D',
-              '@SP',
-              'M=M+1',
-              '// push constant 9',
-              '@9',
-              'D=A',
-              '@SP',
-              'A=M',
-              'M=D',
-              '@SP',
-              'M=M+1',
-            ]
-          )
+        it 'converts it to assembly' do
+          expect(code_writer.call(parsed_code))
+            .to eq(
+              [
+                '// add',
+                '@SP',
+                'M=M-1',
+                'A=M',
+                'D=M',
+                'A=A-1',
+                'M=D+M',
+              ]
+            )
+        end
       end
     end
   end
