23dadaf42784a81426ff81b599c44f068f603a37
rory <rhrg500@gmail.com>
2019-05-12 11:54:02 +0100

    Refactor tests to improve nesting

--- spec/code_writer_spec.rb
+++ spec/code_writer_spec.rb
@@ -3,8 +3,8 @@ require 'code_writer'
 RSpec.describe CodeWriter do
   subject(:code_writer) { CodeWriter }
   describe '#call' do
-    context 'memory access commands' do
-      context 'single push constant' do
+    context 'push commands' do
+      context 'push constant' do
         let(:parsed_code) { ['push constant 8'] }
 
         it 'converts it to assembly' do
@@ -23,35 +23,9 @@ RSpec.describe CodeWriter do
             )
         end
       end
+    end
 
-      context 'mulitple push constants' do
-        let(:parsed_code) { ['push constant 8', 'push constant 9'] }
-
-        it 'converts it to assembly' do
-          expect(code_writer.call(parsed_code))
-            .to eq(
-              [
-                '// push constant 8',
-                '@8',
-                'D=A',
-                '@SP',
-                'A=M',
-                'M=D',
-                '@SP',
-                'M=M+1',
-                '// push constant 9',
-                '@9',
-                'D=A',
-                '@SP',
-                'A=M',
-                'M=D',
-                '@SP',
-                'M=M+1',
-              ]
-            )
-        end
-      end
-
+    context 'pop commands' do
       context 'pop local' do
         let(:parsed_code) { ['pop local 3'] }
 
@@ -79,24 +53,22 @@ RSpec.describe CodeWriter do
       end
     end
 
-    context 'arithmetic commands' do
-      context 'add' do
+    context 'add' do
       let(:parsed_code) { ['add'] }
 
-        it 'converts it to assembly' do
-          expect(code_writer.call(parsed_code))
-            .to eq(
-              [
-                '// add',
-                '@SP',
-                'M=M-1',
-                'A=M',
-                'D=M',
-                'A=A-1',
-                'M=D+M',
-              ]
-            )
-        end
+      it 'converts it to assembly' do
+        expect(code_writer.call(parsed_code))
+          .to eq(
+            [
+              '// add',
+              '@SP',
+              'M=M-1',
+              'A=M',
+              'D=M',
+              'A=A-1',
+              'M=D+M',
+            ]
+          )
       end
     end
   end
